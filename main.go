217f4a3b3b36c98ec27368665616cd90d39afb6e
suzakutakumi <suzakutakumi3@gmail.com>
2022-07-29 20:07:27 +0900

    Hello World

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	r := gin.Default()
+	r.GET("/", index)
+	r.Run(":8080")
+}
+func index(c *gin.Context) {
+	c.String(200, "Hello World")
 }
